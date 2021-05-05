b9f532ba9ac22d827d7589926bf1fd99724ad0b6
Corey Psoinos <corey@streetsmarts.io>
2021-05-05 08:46:48 -0400

    make datetime regexp more forgiving

--- lib/bouncie/entity.rb
+++ lib/bouncie/entity.rb
@@ -35,7 +35,7 @@ module Bouncie
         Bouncie::Entity.new(value)
       elsif value.is_a?(Array)
         value.map { |v| massage_value(v) }
-      elsif value.is_a?(String) && value.match?(/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)((-(\d{2}):(\d{2})|Z)?)$/)
+      elsif value.is_a?(String) && value.match?(/^(\d{4})-(\d{2})-(\d{2})(T|\s)(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)((-(\d{2}):(\d{2})|Z)?)$/)
         DateTime.parse(value)
       else
         value
